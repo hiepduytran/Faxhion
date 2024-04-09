8b31355a8a73dbb01f8f7e5841b40555154136d4
Trần Duy Hiệp <116613860+hiepduytran@users.noreply.github.com>
2024-04-09 17:21:58 +0700

    Remove unnecessary comments

--- frontend/src/Pages/OrderList.jsx
+++ frontend/src/Pages/OrderList.jsx
@@ -32,26 +32,17 @@ const OrderList = () => {
 
   return (
     <div className="order-list-container">
-      {" "}
-      {/* Thêm class cho container */}
-      <h1 className="order-list-title">Order List</h1>{" "}
-      {/* Thêm class cho tiêu đề */}
+      <h1 className="order-list-title">Order List</h1>
       <ul className="order-list">
         {orders.map((order, index) => (
           <li key={index} className="order-item">
-            {" "}
-            {/* Thêm class cho mỗi mục */}
-            <Link
-              to={`/order_detail/${order._id}`}
-              className="order-link" // Thêm class cho link
-            >
+            <Link to={`/order_detail/${order._id}`} className="order-link">
               <div className="order-info">
-                {" "}
-                {/* Thêm class cho thông tin đơn hàng */}
-                <h2>Order ID: {order._id}</h2>{" "}
-                {/* Thêm class cho tiêu đề đơn hàng */}
-                <p className="order-details">Total: {order.total}</p>{" "}
-                {/* Thêm class cho thông tin chi tiết đơn hàng */}
+                <h2>Order ID: {order._id}</h2>
+                <p className="order-details">
+                  <span>Total: {order.total},</span>
+                  <span> status: {order.status}</span>
+                </p>
               </div>
             </Link>
           </li>
